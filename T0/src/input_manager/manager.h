ba7cfb9d23dad75d43c3a2ce370a5275a4863643
anomvlito <fgortega@uc.cl>
2024-08-19 15:54:54 -0400

    structs-arreglar

--- T0/src/input_manager/manager.h
+++ T0/src/input_manager/manager.h
@@ -5,6 +5,21 @@
 #define MAX_SPLIT 255
 #define BUFFER_SIZE 4096
 
+
+// typedef struct lrsh {             
+//   bool consola_activa;
+//   Proceso *procesos;                        
+// } Lrsh;
+
+// typedef struct proceso {             
+//   bool activo;
+//   proceso *proceso_padre;                                  
+//   proceso *proceso_hijo;                                        
+// } Proceso;
+
+
+
 // Declare functions
+
 char **read_user_input();
 void free_user_input(char **input);
